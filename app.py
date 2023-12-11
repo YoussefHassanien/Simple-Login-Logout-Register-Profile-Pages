f9c9431ac77db16b1fff8c7d2d9c294254024e7a
youssefhassanein <youssefhassanein17@gmail.com>
2023-12-11 16:02:17 +0200

    Profile page backend is finished, page styling and css need to be done

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
-from flask import Flask, render_template,request,redirect,session
-import psycopg2
+from flask import Flask, render_template, request, redirect, session
 import psycopg2.extras
 
 app = Flask(__name__)
-app.secret_key = 'queensqueries'
+app.secret_key = "queensqueries"
 
 database_session = psycopg2.connect(
      database="profiledata",
      port=5432,
      host="localhost",
      user="postgres",
-     password="8383"
+     password="Youssef.17.11"
 )
 
 cursor = database_session.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("Profile.html")
+    message = "Please tell us more about you!"
+    activity1 = "I love Rebecca!"
+    activity2 = "I love Rebecca!"
+    activity3 = "I love Rebecca!"
+    userprofileimage = "../static/Images/44-facts-about-rebecca-ferguson-1690782435.jpg"
+    return render_template("Profile.html", infomessage=message, activity1=activity1,
+                           activity2=activity2, activity3=activity3, userprofileimage=userprofileimage)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -27,16 +32,14 @@ def login():
     useremail = request.form.get("email")
     userpassword = request.form.get("password")
     if useremail:
-        cursor.execute('SELECT userid, fname FROM usersinformation where email = %s and password = %s', (useremail, userpassword))
+        cursor.execute('SELECT * FROM usersinformation where email = %s and password = %s', (useremail, userpassword))
         result = cursor.fetchone()
         if result:
             session['user'] = dict(result)
             return redirect('/')
-            # return render_template('index.html', user=dict(result))
-            pass
         else:
             message = 'Please enter correct email and password'
-    return render_template('login.html', msg=message)
+    return render_template('Login.html', msg=message)
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -59,17 +62,26 @@ def register():
         if cursor.fetchone():
             message = 'Account already exits!'
         else:
-            cursor.execute('INSERT INTO usersinformation(Fname, Lname, phone, Address, Email, password, Job, Facebook, Github, Instagram, Linkedin) VALUES (%s, %s ,%s, %s, %s, %s, %s, %s, %s, %s, %s)', (userfname, userlname, userphone, useraddress, useremail, userpassword, userjob, userfacebook, usergithub, userinstagram, userlinkedin))
+            cursor.execute('INSERT INTO usersinformation(Fname, Lname, phone, Address, Email, password, Job, '
+                           'Facebook, Github, Instagram, Linkedin) VALUES (%s, %s ,%s, %s, %s, %s, %s, %s, %s, %s, '
+                           '%s)', (userfname, userlname, userphone, useraddress, useremail, userpassword, userjob,
+                                   userfacebook, usergithub, userinstagram, userlinkedin))
             database_session.commit()
             message = 'You have successfully registered!'
 
+    return render_template("Registration.html", msg=message)
 
-    return render_template("registration.html", msg=message)
 
-
-@app.route("/logout")
+@app.route("/logout", methods=["GET", "POST"])
 def logout():
-    return render_template("login.html")
+    session['user'] = None
+    return redirect('/login')
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit_profile():
+    return render_template("Edit Information.html")
+
 
 if __name__ == '__main__':
     app.run()
